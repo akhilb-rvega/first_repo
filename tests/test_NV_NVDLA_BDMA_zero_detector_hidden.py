0d21a50c3cda4c1e3594c5550eeccb3d87a14ace
akhilb-rvega <akhil.b@rvega.co.in>
2026-01-27 22:15:15 +0530

    Update test_NV_NVDLA_BDMA_zero_detector_hidden.py

--- tests/test_NV_NVDLA_BDMA_zero_detector_hidden.py
+++ tests/test_NV_NVDLA_BDMA_zero_detector_hidden.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import random
 from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, Timer, with_timeout
-from cocotb.result import TestFailure
+from cocotb.triggers import RisingEdge, Timer
 
 from cocotb_test.simulator import run
+from cocotb_tools.runner import get_runner
 
 
 # =============================================================================
